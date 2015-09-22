ec3f6cb14adc88772f0f7a243940d007b641ad3d
unknown <4068886@HS222-14.vtsd.com>
2015-09-22 11:52:41 -0400

    My first commit

---
+++ Lab02Cv100.java
@@ -0,0 +1,28 @@
+// Lab02Cv100.java
+// The 100-point version of Lab 02C is an exercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file.
+
+public class Lab02Cv100
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("Lab 02C, 100 Point Version");
+		System.out.println();
+		System.out.println("First Java Program");
+		System.out.println();
+		System.out.println("Measure today's knowledge ");
+		System.out.println("by yesterday's confusion.");
+		System.out.println();
+		System.out.println("Bewilderment + Exposure = Obvious");
+		System.out.println();
+		System.out.println("# ##### #####   ##### ##### # #   # ##### #####");
+		System.out.println("# #   # #   #   #   # #   # # ##  #   #   #    ");
+		System.out.println("# #   # #   #   ##### #   # # # # #   #   #####");
+		System.out.println("# #   # #   #   #     #   # # #  ##   #       #");
+		System.out.println("# ##### #####   #     ##### # #   #   #   #####");
+	}
+}
+
+
+
